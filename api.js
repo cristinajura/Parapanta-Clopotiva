d7aa9014f2af63fa268030ec5c33318e64cfe249
Cristina Jura <cristina.jura16@gmail.com>
2022-12-28 17:06:07 +0200

    update

--- api.js
+++ api.js
@@ -1,2 +1,2 @@
-export const WEATHER_API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
+export const WEATHER_API_KEY = "61501eba3c254476648ad7c5c4d63aa2";
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
